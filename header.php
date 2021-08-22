ea8355aed51a6cd497bccb3fee885d4ae68a71cc
lumatasia98 <79668405+lumatasia98@users.noreply.github.com>
2021-08-22 15:39:12 -0400

    Update header.php

--- header.php
+++ header.php
@@ -6,3 +6,10 @@
 	<meta name="author" content="Samantha Schiff">
 	<meta name="viewport" width=device-width, initial-scale=1.0">
 </head>
+<style>
+	body {
+		font-family: Verdana;
+		background-color: powderblue;
+		margin: 0;
+	}
+</style>

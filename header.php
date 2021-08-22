beae750bd6179ab200c3b37c3dfadf014a34ddc2
lumatasia98 <79668405+lumatasia98@users.noreply.github.com>
2021-08-22 15:18:41 -0400

    Update header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<html lang="en">
+<head>
+	<title>Samantha Schiff</title>
+	<meta name="description" content="personal website">
+	<meta charset="UTF-8">
+	<meta name="author" content="Samantha Schiff">
+	<meta name="viewport" width=device-width, initial-scale=1.0">
+</head>
